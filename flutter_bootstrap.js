16d4ccc20e8c560eda23e67cdd5769a37b604822
zzhmrtv <zzhmrtv@users.noreply.github.com>
2025-12-17 00:16:42 +0000

    deploy: add75a23c39d9ee2ccdda8a1c79fedb50b9f730a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4033902224"
+    serviceWorkerVersion: "1240352006"
   }
 });
